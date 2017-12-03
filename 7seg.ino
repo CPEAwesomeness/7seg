8f304ddacb72148cddcdb7c16eaa0339e957cca2
Delsorou <delsorou@gmail.com>
2017-12-03 15:07:19 -0800

    Initial commit.

---
+++ 7seg.ino
@@ -0,0 +1,47 @@
+bool BCD[10][4] = {
+  {0, 0, 0, 0},
+  {0, 0, 0, 1},
+  {0, 0, 1, 0},
+  {0, 0, 1, 1},
+  {0, 1, 0, 0},
+  {0, 1, 0, 1},
+  {0, 1, 1, 0},
+  {0, 1, 1, 1},
+  {1, 0, 0, 0},
+  {1, 0, 0, 1}
+};
+
+char ones[4] = {
+  A0,
+  A1,
+  A2,
+  A3
+};
+
+char tens[4] = {
+  10,
+  16,
+  14,
+  15
+};
+
+void output_digit(char* place, bool* digit) {
+  for (char i = 0; i < 4; ++i) {
+    digitalWrite(place[i], digit[i]);
+  };
+}
+
+void setup() {
+  for (char i = 0; i < 4; ++i) {
+    pinMode(ones[i], OUTPUT);
+    pinMode(tens[i], OUTPUT);
+  };
+}
+
+void loop() {
+  for (char i = 0; i < 100; ++i) {
+    output_digit(tens, BCD[i / 10]);
+    output_digit(ones, BCD[i % 10]);
+    delay(166);
+  }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# 7seg
+
+> 7 Segment Display demonstration project for CPE201, Fall 2017
+
+
+## Developers:
+* Jessica Mccrite ([jessicamccrite](http://github.com/jessicamccrite/))
+* Keegan Phillips ([kikichan1212](http://github.com/kikichan1212/))
+* Aaryna Irwin ([delsorou](http://github.com/Delsorou/))
+
+## What?:
+We had to digitally design a thing, and this is the thing.
+
+It counts from 00 to 99 on two seven-segment displays. We integrated an
+Arduino Pro Micro into the project board in order to provide timing functions
+for the project's hardware, as well as a firmware backup to demonstrate
+creating a hardware-equivalent programmed state machine.
