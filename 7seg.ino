f75e609e5901d05a905edb74abf673bf4c229d1f
Delsorou <delsorou@gmail.com>
2017-12-03 19:33:53 -0800

    comment housekeeping

--- 7seg.ino
+++ 7seg.ino
@@ -3,32 +3,32 @@ const int counter_interval = 167;
 
 // BCD definition table
 const bool BCD[10][4] = {
-  {0, 0, 0, 0},
-  {0, 0, 0, 1},
-  {0, 0, 1, 0},
-  {0, 0, 1, 1},
-  {0, 1, 0, 0},
-  {0, 1, 0, 1},
-  {0, 1, 1, 0},
-  {0, 1, 1, 1},
-  {1, 0, 0, 0},
-  {1, 0, 0, 1}
+	{0, 0, 0, 0},
+	{0, 0, 0, 1},
+	{0, 0, 1, 0},
+	{0, 0, 1, 1},
+	{0, 1, 0, 0},
+	{0, 1, 0, 1},
+	{0, 1, 1, 0},
+	{0, 1, 1, 1},
+	{1, 0, 0, 0},
+	{1, 0, 0, 1}
 };
 
 // Output ports for ones place
 const char ones[4] = {
-  A0,
-  A1,
-  A2,
-  A3
+	A0,
+	A1,
+	A2,
+	A3
 };
 
 // Output ports for tens place
 const char tens[4] = {
-  10,
-  16,
-  14,
-  15
+	10,
+	16,
+	14,
+	15
 };
 
 // Global state variables
@@ -36,32 +36,36 @@ unsigned long counter_last = 0;
 unsigned char ones_last = 0;
 unsigned char tens_last = 0;
 
+// This function prints a BCD code to one of the port sequences via loop
 void outputDigit(const char* place, const bool* digit) {
-  for (unsigned char i = 0; i < 4; ++i) {
-    digitalWrite(place[i], digit[i]);
-  };
+	for (unsigned char i = 0; i < 4; ++i) digitalWrite(place[i], digit[i]);
 }
 
 void setup() {
-  for (unsigned char i = 0; i < 4; ++i) {
-    pinMode(ones[i], OUTPUT);
-    pinMode(tens[i], OUTPUT);
-  };
+	// This loop initializes the port pins we chose as outputs
+	for (unsigned char i = 0; i < 4; ++i) {
+		pinMode(ones[i], OUTPUT);
+		pinMode(tens[i], OUTPUT);
+	};
 }
 
 void loop() {
-  unsigned long counter_current = millis();
-  if (counter_current >= counter_last + counter_interval) {
-    outputDigit(ones, BCD[ones_last]);
-    outputDigit(tens, BCD[tens_last]);
+	// This section is controlled by the counter_interval timing
+	unsigned long counter_current = millis();
+	if (counter_current >= counter_last + counter_interval) {
+		// Update the output pins
+		outputDigit(ones, BCD[ones_last]);
+		outputDigit(tens, BCD[tens_last]);
 
-    if (ones_last == 9) {
-      if (tens_last == 9) tens_last = 0;
-      else ++tens_last;
-      ones_last = 0;
-    }
-    else ++ones_last;
-        
-    counter_last = counter_current;
-  }
+		// This conditional section handles carrys and resets
+		if (ones_last == 9) {
+			if (tens_last == 9) tens_last = 0;
+			else ++tens_last;
+			ones_last = 0;
+		}
+		else ++ones_last;
+
+		// Reset the counter timer
+		counter_last = counter_current;
+	}
 }
